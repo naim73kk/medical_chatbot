8ad824c755746381c5c4b5f3a56af512021f1326
naim73kk <maouchenaim73@gmail.com>
2025-11-05 16:11:10 +0100

    install dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name="medical_chatbot",
+    version="1.0.0",
+    author="Naim Maouche"
+    author_email="maouchen73@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)

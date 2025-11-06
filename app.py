899cea02a065301e2b2e3828162d5e038aa04656
naim73kk <maouchenaim73@gmail.com>
2025-11-06 15:17:12 +0100

    update code

--- app.py
+++ app.py
@@ -69,7 +69,6 @@ def chat():
     print("Response : ", response["answer"])
     return str(response["answer"])
 
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render will set PORT
+    app.run(host="0.0.0.0", port=port)
